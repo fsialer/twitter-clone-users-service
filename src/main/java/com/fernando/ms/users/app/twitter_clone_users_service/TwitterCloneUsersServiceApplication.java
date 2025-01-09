c1690165f3b82b58e8d35bb74559d7ff83da1ee7
Fernando <asialer05@hotmail.com>
2025-01-09 10:34:45 -0500

    Proyecto base

---
+++ src/main/java/com/fernando/ms/users/app/twitter_clone_users_service/TwitterCloneUsersServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.ms.users.app.twitter_clone_users_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterCloneUsersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterCloneUsersServiceApplication.class, args);
+	}
+
+}
